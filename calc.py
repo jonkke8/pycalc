2e55a6528aa61695269727c198309ee9572197a8
jonkke8 <45978346+jonkke8@users.noreply.github.com>
2021-09-26 15:37:01 +0300

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,16 @@
+print("Very good calculator yes.")
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+val = input("'")
+if val.isalpha():
+    print(add)
